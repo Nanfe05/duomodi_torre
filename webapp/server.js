7dd016adf61aa78808d9d373b0d129628d60f71b
Nanfe05 <hernan05felipe@gmail.com>
2020-06-18 21:13:08 -0500

    Dependencies to Deploy

--- webapp/server.js
+++ webapp/server.js
@@ -12,9 +12,9 @@ app.use('/opportunities',createProxyMiddleware({target:'https://search.torre.co/
 
 app.use(express.static(path.join(__dirname,'build')));
 
-// app.get('*',(req,res)=>{
-//     res.sendFile(path.join(__dirname+'/build/index.html'));
-// });
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 app.listen(port,()=>{
     console.log('App Started');
